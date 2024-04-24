6bf40eecc372f2b9d39ecc72a5f4f4468e5bca56
sim yoonji <124511094+simyoonji@users.noreply.github.com>
2024-04-25 00:40:15 +0900

    sfsegsfdf

--- script/scroll-animation.js
+++ script/scroll-animation.js
@@ -1,18 +1,8 @@
 const main = document.querySelector('#main');
-const title = document.querySelector('.title');
 const profile = document.querySelector('#profile');
 const profileLeft = profile.querySelector('.profile-about ul');
 const profileTop = profile.querySelector('.profile-txt-top');
 const profileBt = profile.querySelector('.profile-txt-bottom');
-
-gsap.to(title, {
-	duration: 1,
-	opacity : 1,
-
-	scrollTrigger: {
-		trigger : main,
-	}
-});
 
 gsap.to(profileLeft, {
 	duration: 1,
