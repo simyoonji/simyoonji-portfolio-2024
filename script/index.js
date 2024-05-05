fe11fd63ffbce244300edf3abb2c220b19034e52
sim yoonji <124511094+simyoonji@users.noreply.github.com>
2024-05-05 18:54:07 +0900

    check10

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
+// loading
+window.addEventListener('load', () => {
+    var loading = document.querySelector('.loading');
+    loading.style.display = 'none';
+});
+
 const mouse = document.querySelector('.mouse');
 const buttonAll = document.querySelectorAll('button');
 const aAll = document.querySelectorAll('a');
